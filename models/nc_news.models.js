d136b782a955baaaa5c713e88b58487616f29b42
SaraGarciaM <saragarciamatos@gmail.com>
2024-04-16 16:37:29 +0100

    Adds controller/model for delete /api/comment/comment_id endpoint

--- models/nc_news.models.js
+++ models/nc_news.models.js
@@ -91,6 +91,19 @@ function updateArticleById(article_id, { inc_votes }) {
     });
 }
 
+function removeCommentById(comment_id) {
+  return db
+    .query(`SELECT * FROM comments WHERE comment_id = $1`, [comment_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, message: "Comment not found." });
+      }
+      return db.query(`DELETE FROM comments WHERE comment_id = $1`, [
+        comment_id,
+      ]);
+    });
+}
+
 module.exports = {
   fetchAllTopics,
   fetchArticleById,
@@ -99,4 +112,5 @@ module.exports = {
   checkIfArticleExists,
   addCommentByArticleId,
   updateArticleById,
+  removeCommentById,
 };
